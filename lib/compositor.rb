009a1ac7992546a4461e051e75d3198f26918135
Paul Henry and Richard Millan, richardiux <pair+paul+richard@wanelo.com>
2013-05-29 16:27:55 -0700

    Only modifying String class if constantize and underscore aren't defined.

--- lib/compositor.rb
+++ lib/compositor.rb
@@ -10,22 +10,28 @@ module Compositor
   end
 end
 
-class String
-  def constantize
-    camel_cased_word = self
-    unless /\A(?:::)?([A-Z]\w*(?:::[A-Z]\w*)*)\z/ =~ camel_cased_word
-      raise NameError, "#{camel_cased_word.inspect} is not a valid constant name!"
+unless "".respond_to?(:constantize)
+  class String
+    def constantize
+      camel_cased_word = self
+      unless /\A(?:::)?([A-Z]\w*(?:::[A-Z]\w*)*)\z/ =~ camel_cased_word
+        raise NameError, "#{camel_cased_word.inspect} is not a valid constant name!"
+      end
+
+      Object.module_eval("::#{$1}", __FILE__, __LINE__)
     end
-
-    Object.module_eval("::#{$1}", __FILE__, __LINE__)
   end
+end
 
-  def underscore
-    self.gsub(/::/, '/').
+unless "".respond_to?(:underscore)
+  class String
+    def underscore
+      self.gsub(/::/, '/').
         gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
         gsub(/([a-z\d])([A-Z])/, '\1_\2').
         tr("-", "_").
         downcase
+    end
   end
 end
 
